54405ef0c50479f1078e18c34efb36556d846295
halovivek <halovivek@gmail.com>
2022-08-23 11:31:15 +0530

    update

---
+++ 22082022_practice3.py
@@ -0,0 +1,19 @@
+class Fraction:
+    def __init__(self,nr,dr = 1):
+        self.nr = nr
+        self.dr = dr
+        if self.dr < 0:
+            self.nr *= -1
+            self.dr *= -1
+
+    def show(self):
+        print(f'{self.nr}/{self.nr}')
+
+
+f1 = Fraction(2,3)
+f1.show()
+f2 = Fraction(3,8)
+f2.show()
+f3 = Fraction(-5,-6)
+f3.show()
+
---
+++ test040082022.py
@@ -0,0 +1,4 @@
+l = [1, 2, 3,4, 5]
+k = [33,44,55,66,77]
+d = dict.fromkeys(l,k)
+print(d)
